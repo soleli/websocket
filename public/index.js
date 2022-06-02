8e1a9b47028d7a174474436d2b7c05985edd8eff
Soledad Alaniz <sole_lr@hotmail.com>
2022-06-02 13:16:04 -0300

    agrego funcionalidad para capturar datos en js

--- public/index.js
+++ public/index.js
@@ -1,1 +1,16 @@
 const socket = io();
+const productForm = document.getElementById('productForm')
+
+productForm.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    const title = document.getElementById('title')
+    const price = document.getElementById('price')
+    const image = document.getElementById('image')
+  
+    const product = {
+      title: title.value,
+      price: price.value,
+      image: image.value,
+    }
+    console.log(product)
+})
